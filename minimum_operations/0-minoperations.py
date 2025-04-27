99771d907d88767349c27713c5650ea96ed39a01
Anomaliszt <xavier.sanchez44@orange.fr>
2025-04-27 23:09:35 +0200

    Refactor docstrings in minoperations.py for improved readability

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 """
-Module to calculate the minimum number of operations needed to achieve a target number of 'H' characters.
+Module to calculate the minimum number of operations needed to achieve 
+a target number of 'H' characters.
 """
 
 
 def minOperations(n):
     """
-    Calculates the fewest number of operations needed to result in exactly n H characters.
+    Calculates the fewest number of operations needed to result in 
+    exactly n H characters.
 
     Args:
         n (int): The target number of 'H' characters.
 
     Returns:
-        int: The minimum number of operations needed. Returns 0 if n is impossible to achieve.
+        int: The minimum number of operations needed. Returns 0 if n is 
+        impossible to achieve.
     """
     if n <= 1:
         return 0
