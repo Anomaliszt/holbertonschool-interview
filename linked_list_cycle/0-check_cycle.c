2d4623d35623ddc4d634375d0866ce39f0b97757
Anomaliszt <xavier.sanchez44@orange.fr>
2025-05-25 22:28:32 +0200

    Implement check_cycle function to detect cycles in a singly linked list; add lists.h header file for list structure definitions.

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it
+ * @list: pointer to the head of the list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+    listint_t *slow_ptr, *fast_ptr;
+
+    if (list == NULL || list->next == NULL)
+        return (0);
+
+    slow_ptr = list;
+    fast_ptr = list->next;
+
+    while (fast_ptr != NULL && fast_ptr->next != NULL)
+    {
+        if (slow_ptr == fast_ptr)
+            return (1);
+        slow_ptr = slow_ptr->next;
+        fast_ptr = fast_ptr->next->next;
+    }
+
+    return (0);
+}
