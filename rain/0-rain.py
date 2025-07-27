a863464eee5e5ec0b1bd413fa2e7fe354da71fa3
Anomaliszt <xavier.sanchez44@orange.fr>
2025-07-27 17:56:21 +0200

    Refactor docstring formatting and improve readability in rain function

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -3,7 +3,8 @@
 
 
 def rain(walls):
-    """Calculate how many square units of water will be retained after it rains.
+    """Calculate how many square units of water will be retained after it
+    rains.
 
     Args:
         walls: list of non-negative integers representing wall heights.
@@ -13,24 +14,24 @@ def rain(walls):
     """
     if not walls or len(walls) < 3:
         return 0
-    
+
     n = len(walls)
     water_trapped = 0
-    
+
     left_max = [0] * n
     right_max = [0] * n
-    
+
     left_max[0] = walls[0]
     for i in range(1, n):
         left_max[i] = max(left_max[i-1], walls[i])
-    
+
     right_max[n-1] = walls[n-1]
     for i in range(n-2, -1, -1):
         right_max[i] = max(right_max[i+1], walls[i])
-    
+
     for i in range(n):
         water_level = min(left_max[i], right_max[i])
         if water_level > walls[i]:
             water_trapped += water_level - walls[i]
-    
+
     return water_trapped
