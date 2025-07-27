f03032ba674f24b78ac6c8a1ea5de3368804db38
Anomaliszt <xavier.sanchez44@orange.fr>
2025-07-27 17:55:14 +0200

    Implement rainwater trapping algorithm in 0-rain.py and add initial README.md

---
+++ rain/0-rain.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Rain water trapping problem."""
+
+
+def rain(walls):
+    """Calculate how many square units of water will be retained after it rains.
+
+    Args:
+        walls: list of non-negative integers representing wall heights.
+
+    Returns:
+        Integer indicating total amount of rainwater retained.
+    """
+    if not walls or len(walls) < 3:
+        return 0
+    
+    n = len(walls)
+    water_trapped = 0
+    
+    left_max = [0] * n
+    right_max = [0] * n
+    
+    left_max[0] = walls[0]
+    for i in range(1, n):
+        left_max[i] = max(left_max[i-1], walls[i])
+    
+    right_max[n-1] = walls[n-1]
+    for i in range(n-2, -1, -1):
+        right_max[i] = max(right_max[i+1], walls[i])
+    
+    for i in range(n):
+        water_level = min(left_max[i], right_max[i])
+        if water_level > walls[i]:
+            water_trapped += water_level - walls[i]
+    
+    return water_trapped
