38a0d8088ea5a057864d05b8f00307c1537b1192
Anomaliszt <xavier.sanchez44@orange.fr>
2025-09-03 17:45:31 +0200

    Fix formatting by removing unnecessary blank lines in rotate_2d_matrix function

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -7,10 +7,10 @@ Rotate 2D Matrix
 def rotate_2d_matrix(matrix):
     """
     Rotate an n x n 2D matrix 90 degrees clockwise in-place.
-    
+
     Args:
         matrix: n x n 2D matrix to rotate
-    
+
     Returns:
         None (matrix is modified in-place)
     """
@@ -19,6 +19,6 @@ def rotate_2d_matrix(matrix):
     for i in range(n):
         for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-    
+
     for i in range(n):
         matrix[i].reverse()
